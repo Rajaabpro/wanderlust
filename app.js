865963d1798887c19eaf1624a58bcfc5f644a265
Rajaabpro <rajaab.pro@gmail.com>
2025-09-07 15:01:55 +0500

    feat: initialize Express app with MongoDB connection and basic route setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const MONGO_URI = "mongodb://localhost:27017/wanderlust";
+main().then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URI);
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+    
