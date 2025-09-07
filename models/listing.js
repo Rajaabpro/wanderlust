0becf56a446bff63f088b78fa168cc7fbabebee2
Rajaabpro <rajaab.pro@gmail.com>
2025-09-07 16:29:30 +0500

    feat: add required title field and default image handling in listing schema

--- models/listing.js
+++ models/listing.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const defaultImage = "https://unsplash.com/photos/golden-mountain-peaks-at-sunset-with-dramatic-clouds-IyhdFcaRYqE";
 const listingSchema = new Schema({
-    title: String,
+    title:{
+        type: String, 
+        required: true
+    },
     description: String,
-    image: String,
+    image:{
+        type: String,
+        default: defaultImage,
+        set: (v) => v === "" ? defaultImage : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
+ 
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
     
