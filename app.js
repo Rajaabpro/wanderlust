b3396e85006e1f7c6cfa3493ee683f5ec7e9c1e1
Rajaabpro <rajaab.pro@gmail.com>
2025-09-08 18:12:15 +0500

    feat: implement routes for listing index and details views, configure view engine

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const Listing = require("./models/listing");
-
+const path = require("path");
 
 const MONGO_URI = "mongodb://localhost:27017/wanderlust";
 main().then(() => {
@@ -16,22 +16,38 @@ async function main() {
     await mongoose.connect(MONGO_URI);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.send("Hi im root");
 });
-
-app.get("/testListings", async (req, res) => {
-    let samleListings = new Listing({
-        title: "Sample Listing 1",
-        description: "This is a sample listing 1",
-        image: "https://via.placeholder.com/150",
-        price: 100,
-        location: "New York",
-        country: "USA"
-    });
-    await samleListings.save();
-    res.send("Listings saved to database successfully");
+//index Route
+app.get("/Listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings})
 });
+
+//show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+// app.get("/testListings", async (req, res) => {
+//     let samleListings = new Listing({
+//         title: "Sample Listing 1",
+//         description: "This is a sample listing 1",
+//         image: "https://via.placeholder.com/150",
+//         price: 100,
+//         location: "New York",
+//         country: "USA"
+//     });
+//     await samleListings.save();
+//     res.send("Listings saved to database successfully");
+// });
     
 app.listen(3000, () => {
     console.log("Server started on port 3000");
