ca74473714a3899b94434b5efea0835f85656ea3
Rajaabpro <rajaab.pro@gmail.com>
2025-09-15 19:42:58 +0500

    feat: update listing schema to allow optional image and add location and country fields

--- schema.js
+++ schema.js
@@ -4,8 +4,10 @@ const listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        image: Joi.string().required(),
-        price: Joi.number().required(),
+        image: Joi.string().allow("", null),
+        price: Joi.number().min(0).required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
     }).required()
 });
 
