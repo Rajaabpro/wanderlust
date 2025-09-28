f6c96f3d41670ce70c5d62aed733e950e93f0c9d
Rajaabpro <rajaab.pro@gmail.com>
2025-09-28 11:33:36 +0500

    feat: implement review creation functionality in reviews route, enabling users to submit reviews for listings

---
+++ routes/reviews.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Review = require("../models/review.js");
+const { reviewSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) throw new ExpressError(400, error.message);
+    next();
+};
+
+router.post("/", validateReview, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    let { error } = reviewSchema.validate(req.body);
+    if (error) throw new ExpressError(400, error.message);
+    let newReview = new Review(req.body.review);
+    await newReview.save();
+    listing.reviews.push(newReview);
+    await listing.save();
+    res.redirect(`/listings/${id}`);
+}));
+module.exports = router;
