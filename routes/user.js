dadbe12d2edd6f9464496700d575071748a0f95a
Rajaabpro <rajaab.pro@gmail.com>
2025-10-03 21:38:35 +0500

    feat: enhance user signup and login routes with error handling and async wrapper

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,32 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup");
 });
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
     let { username, email, password } = req.body;
     const newUser = new User({ username, email });
     const registeredUser = await User.register(newUser, password);
     console.log(registeredUser);
     req.flash("success", "Welcome to Wanderlust");
+    res.redirect("/listings");
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", { failureRedirect: "/login" }), (req, res) => {
+    req.flash("success", "Welcome back");
     res.redirect("/listings");
 });
 
