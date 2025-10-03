d1b51d7c9b4fd56273ecea94b7c00b03d0926a92
Rajaabpro <rajaab.pro@gmail.com>
2025-10-03 21:25:14 +0500

    feat: implement user signup POST route to handle registration and success messaging

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,13 @@ router.get("/signup", (req, res) => {
     res.render("users/signup");
 });
 
+router.post("/signup", async (req, res) => {
+    let { username, email, password } = req.body;
+    const newUser = new User({ username, email });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to Wanderlust");
+    res.redirect("/listings");
+});
 
 module.exports = router;
