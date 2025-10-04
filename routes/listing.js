f9fafe0339dadba4a476d089c357eedb035ef2f7
Rajaabpro <rajaab.pro@gmail.com>
2025-10-04 10:11:39 +0500

    feat: apply authentication middleware to restrict access to new listing creation route

--- routes/listing.js
+++ routes/listing.js
@@ -5,13 +5,15 @@ const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
+const isLoggedIn = require("../middleware.js");
+
 
 router.get("/", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 }));
 
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs");
 });
 
