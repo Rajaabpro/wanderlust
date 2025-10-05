9de56bb55e1d67134e1dbc2844e1cfca7c9534fe
Rajaabpro <rajaab.pro@gmail.com>
2025-10-05 11:58:58 +0500

    feat: add avatar field to user schema for user profile customization

--- models/user.js
+++ models/user.js
@@ -7,6 +7,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    avatar: {
+        type: String,
+        default: null
+    }
 });
 
 userSchema.plugin(passportLocalMongoose);
