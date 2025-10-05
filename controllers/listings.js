04785b4cd83a13174563437c68fe6e658d201334
Rajaabpro <rajaab.pro@gmail.com>
2025-10-05 16:21:46 +0500

    feat: implement complete listings controller with CRUD operations and review management for enhanced functionality

---
+++ controllers/listings.js
@@ -0,0 +1,61 @@
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}
+
+module.exports.new = (req, res) => {
+    res.render("listings/new.ejs");
+}
+
+module.exports.showListing = async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
+    res.render("listings/show.ejs", { listing });
+}
+
+module.exports.editListing = async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}
+
+module.exports.createListing = async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New Listing created successfully");
+    res.redirect("/listings");
+}
+
+module.exports.updateListing = async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+}
+
+module.exports.deleteListing = async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}
+
+module.exports.createReview = async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    const newReview = new Review(req.body.review);
+    await newReview.save();
+    listing.reviews.push(newReview);
+    await listing.save();
+    res.redirect(`/listings/${id}`);
+}
+
+module.exports.deleteReview = async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}
