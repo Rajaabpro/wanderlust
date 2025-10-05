e228a590c615d282114dad1e86bab0d660e88a7f
Rajaabpro <rajaab.pro@gmail.com>
2025-10-05 16:58:40 +0500

    feat: implement user authentication controller with signup, login, and logout functionalities to enhance user management

--- routes/user.js
+++ routes/user.js
@@ -3,43 +3,21 @@ const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const isLoggedIn = require("../middleware.js");
 const { saveRedirectUrl } = require("../middleware.js");
-router.get("/signup", (req, res) => {
-    res.render("users/signup");
-});
+const userController = require("../controllers/users.js");
+const { signup, renderSignupForm, renderLoginForm, login, logout } = userController;
 
-router.post("/signup", wrapAsync(async (req, res) => {
-    try {
-    let { username, email, password } = req.body;
-    const newUser = new User({ username, email });
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Wanderlust");
-    res.redirect("/listings");
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/users/signup");
-    }
-}));
+router.get("/signup", renderSignupForm);
 
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-});
+router.post("/signup", wrapAsync(signup));
 
-router.post("/login",saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/users/login" }), (req, res) => {
-    req.flash("success", "Welcome to Wanderlust again");
-    let redirectUrl = req.session.redirectUrl || "/listings";
-    delete req.session.redirectUrl;
-    res.redirect(redirectUrl);
-});
+router.get("/login", renderLoginForm);
 
-router.get("/logout", (req, res) => {
-    req.logout(err => {
-        if (err) return next(err);
-        req.flash("success", "You have been logged out successfully");
-        res.redirect("/listings");
-    });
-});
+router.post("/login", saveRedirectUrl, passport.authenticate("local",{
+    failureRedirect: "/login",
+    failureFlash: true,
+}), login);
+
+router.get("/logout", logout);
 
 module.exports = router;
