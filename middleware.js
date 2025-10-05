85e3b7189ab6e213c10626a25fb652e5497dcbc0
Rajaabpro <rajaab.pro@gmail.com>
2025-10-05 12:09:20 +0500

    feat: enhance middleware with validation for listings and reviews to ensure data integrity and improve user feedback

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 const Listing = require("./models/listing.js");
-module.exports = (req, res, next) => {
+const { reviewSchema, listingSchema } = require("./schema.js");
+
+module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You must be logged in to do that");
@@ -7,6 +9,7 @@ module.exports = (req, res, next) => {
     }
     next();
 }
+
 
 module.exports.saveRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
@@ -22,6 +25,24 @@ module.exports.isOwner = async (req, res, next) => {
     if (!currentUser || !listing.owner.equals(currentUser._id)) {
         req.flash("error", "You are not authorized to do that");
         return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        req.flash("error", error.message);
+        return res.redirect(`/listings/${req.params.id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        req.flash("error", error.message);
+        return res.redirect(`/listings/${req.params.id}`);
     }
     next();
 }
