690a600b19a1f782f60cc87e529ceea650fd06ea
Rajaabpro <rajaab.pro@gmail.com>
2025-10-05 11:58:02 +0500

    feat: extend middleware functionality to save redirect URL and check listing ownership for enhanced access control

--- middleware.js
+++ middleware.js
@@ -1,7 +1,27 @@
+const Listing = require("./models/listing.js");
 module.exports = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.flash("error", "You must be logged in to create a listing");
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to do that");
         return res.redirect("/users/login");
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+}
+
+module.exports.isOwner = async (req, res, next) => {
+    const { currentUser } = req;
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!currentUser || !listing.owner.equals(currentUser._id)) {
+        req.flash("error", "You are not authorized to do that");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 }
