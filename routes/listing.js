79ab212468a76a4f6dc5303650eec5a78f58a3e6
Rajaabpro <rajaab.pro@gmail.com>
2025-10-07 18:50:59 +0500

    feat: integrate multer for file uploads in listing route with cloud storage configuration

--- routes/listing.js
+++ routes/listing.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { isLoggedIn, validateListing, isOwner } = require("../middleware.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const { index, new: newListing, showListing, createListing, editListing, updateListing, deleteListing, createReview, deleteReview } = require("../controllers/listings.js");
-
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 router.route("/")
     .get(wrapAsync(index))
     .post(isLoggedIn, validateListing, wrapAsync(createListing));
