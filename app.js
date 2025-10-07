7b9e594b3f84e436f80ff62f059c5f6bc79254a2
Rajaabpro <rajaab.pro@gmail.com>
2025-10-07 18:50:28 +0500

    feat: load environment variables in development mode and log the API key for debugging purposes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+console.log(process.env.KEY);
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
