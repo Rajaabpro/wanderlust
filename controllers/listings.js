b18e300a3c45ccf142ab9ec97bf6c294a8ff2a4f
Rajaabpro <rajaab.pro@gmail.com>
2025-10-08 20:20:09 +0500

    feat: enhance listing creation to include image URL and filename for better media management

--- controllers/listings.js
+++ controllers/listings.js
@@ -24,8 +24,12 @@ module.exports.editListing = async (req, res) => {
 }
 
 module.exports.createListing = async (req, res) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    console.log(url,'<<<<<url>>>>',filename,'<<<<<filename>>>>');
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image.url = {url, filename};
     await newListing.save();
     req.flash("success", "New Listing created successfully");
     res.redirect("/listings");
