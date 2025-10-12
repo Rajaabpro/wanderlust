e0ed4689ed1a28d20955f4e53fb9b0a94a08916a
Rajaabpro <rajaab.pro@gmail.com>
2025-10-12 08:30:39 +0500

    refactor: remove image object from listing schema to simplify validation

--- schema.js
+++ schema.js
@@ -4,10 +4,6 @@ const listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
-        image: Joi.object({
-            url: Joi.string().allow("", null),
-            filename: Joi.string().allow("", null)
-        }).allow(null),
         price: Joi.number().min(0).required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
